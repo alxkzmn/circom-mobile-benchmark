f53c9f9fb6b7ecd048090d7743f75ab2d423587c
Alex Kuzmin <alex.kuzmin@proton.me>
2025-02-21 19:38:01 +0800

    Add Sha256 circuit

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 use anyhow::Result;
 use mopro_ffi::{MoproError, ProofCalldata, G1, G2};
-use rust_rapidsnark::groth16_prover_zkey_file_wrapper;
+
+uniffi::include_scaffolding!("mopro");
+
+witnesscalc_adapter::witness!(sha256);
 
 struct GenerateProofResult {
     proof: String,
@@ -11,10 +14,10 @@ fn generate_circom_proof(
     zkey_path: String,
     inputs_json: String,
 ) -> Result<GenerateProofResult, mopro_ffi::MoproError> {
-    let wtns_buffer = multiplier2_witness(inputs_json.as_str())
+    let wtns_buffer = sha256_witness(inputs_json.as_str())
         .map_err(|e| mopro_ffi::MoproError::CircomError(format!("WitGen error: {}", e)))
         .unwrap();
-    let proof = groth16_prover_zkey_file_wrapper(&zkey_path, wtns_buffer)
+    let proof = rust_rapidsnark::groth16_prover_zkey_file_wrapper(&zkey_path, wtns_buffer)
         .map_err(|e| mopro_ffi::MoproError::CircomError(format!("Prover error: {}", e)))
         .unwrap();
     Ok(GenerateProofResult {
@@ -35,6 +38,57 @@ fn verify_circom_proof(
     )
 }
 
-uniffi::include_scaffolding!("mopro");
+#[cfg(test)]
+mod tests {
+    use crate::GenerateProofResult;
+    use anyhow::Result;
+    use rust_rapidsnark::groth16_prover_zkey_file_wrapper;
 
-witnesscalc_adapter::witness!(multiplier2);
+    witnesscalc_adapter::witness!(sha256);
+
+    fn generate_circom_proof(
+        zkey_path: String,
+        inputs_json: String,
+    ) -> Result<GenerateProofResult, mopro_ffi::MoproError> {
+        let wtns_buffer = sha256_witness(inputs_json.as_str())
+            .map_err(|e| mopro_ffi::MoproError::CircomError(format!("WitGen error: {}", e)))
+            .unwrap();
+        let proof = groth16_prover_zkey_file_wrapper(&zkey_path, wtns_buffer)
+            .map_err(|e| mopro_ffi::MoproError::CircomError(format!("Prover error: {}", e)))
+            .unwrap();
+        Ok(GenerateProofResult {
+            proof: proof.proof,
+            inputs: proof.public_signals,
+        })
+    }
+
+    fn verify_circom_proof(
+        vkey: String,
+        proof: String,
+        public_input: String,
+    ) -> Result<bool, mopro_ffi::MoproError> {
+        Ok(
+            rust_rapidsnark::groth16_verify_wrapper(&proof, &public_input, &vkey)
+                .map_err(|e| {
+                    mopro_ffi::MoproError::CircomError(format!("Verification error: {}", e))
+                })
+                .unwrap(),
+        )
+    }
+
+    #[test]
+    fn test_generate_circom_proof() {
+        let zkey_path = "test-vectors/circom/sha256.zkey".to_string();
+        let inputs_json_path = "test-vectors/circom/inputs.json".to_string();
+        let inputs_json = std::fs::read_to_string(inputs_json_path).unwrap();
+        let result = generate_circom_proof(zkey_path, inputs_json).unwrap();
+
+        println!("Proof: {}", result.proof);
+        println!("Inputs: {}", result.inputs);
+
+        let vkey_path = "test-vectors/circom/verification_key".to_string();
+        let vkey = std::fs::read_to_string(vkey_path).unwrap();
+        let result = verify_circom_proof(vkey, result.proof, result.inputs).unwrap();
+        assert_eq!(result, true);
+    }
+}
